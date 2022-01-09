11ec8bcbb91cde3810d4010720afff883758d231
Sai Landgey <slandgey@NextSphere.com>
2022-01-09 22:05:15 +0530

    Added component
    Added sample component for demonstration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipe.component.ts
@@ -0,0 +1,13 @@
+import{Component} from '@angular/core'
+
+@Component({
+    selector:'recipe',
+    template:'<h4>Recipes will load soon...</h4>',
+
+})
+export class RecipeComponent{
+
+}
+
+
+
